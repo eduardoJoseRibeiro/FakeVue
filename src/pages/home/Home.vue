278401d4a338de98c6d491aede44d5f4dc1f9cbe
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2018-06-14 09:28:44 -0300

    Store configurada

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,11 +3,37 @@
         Eu sou a HOME!!!
         <br>
         <router-link :to="{ name : 'posts'}">Posts</router-link>
+        <p>{{textoQualquer}}</p>
+        <form @submit.prevent="novoTexto()">
+            <input type="text" v-model="setTexto">
+            <button type="submit">Salvar</button>
+        </form>
     </div>    
 </template>
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
-    
+    data(){
+        return {
+            setTexto : ''
+        }
+    },
+    methods : {
+        novoTexto(){
+            this.mudatexto(this.setTexto); //Pode ser feito assim
+            // this.$store.commit('mudatexto', this.setTexto); //Ou assim!
+            console.log(this.$store.state.textoQualquer);
+        },
+        ...mapMutations([
+            'mudatexto'
+        ])
+    },
+    computed : {
+        textoQualquer(){
+            return this.$store.state.textoQualquer
+        }
+    }
 }
 </script>
 <style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import VueResource from 'vue-resource'
-import { Post } from '../domain/post/Post'
-//import * as actions from './actions'
-//import * as mutations from './mutations'
+
+export const state = {
+  textoQualquer : 'Panda doidão'  
+};
+
+export const mutations = {
+    mudatexto(state, novoTexto){
+        state.textoQualquer = novoTexto;
+    },
+    pandao(state){
+        state.textoQualquer = 'Pandao';
+    }
+};
+
+export const actions = {
+
+}
 
 Vue.use(Vuex);
-Vue.use(VueResource);
-
-const state = {
-    
-};
-
-const getters = {
-
-};
-
-const mutations = {
-    
-};
-
-const actions = {
-    
-};
 
 export default new Vuex.Store({
     state,
-    mutations,
-    getters,
-    actions
+    mutations
 });
